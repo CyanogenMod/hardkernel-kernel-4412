2f7c1f9b497a1660dab28889a4a4cd6f634c278d
Jeongtae Park <jtp.park@samsung.com>
2010-12-20 23:58:50 +0900

    MFC: Remove plane size check routine for encoder OUTPUT
    This patch removes palne size check routine for encoder OUTPUT.
    
    Change-Id: I57dc19b1f995a1d14dd7602dc8e1e63f79888dd9
    Signed-off-by: Jeongtae Park <jtp.park@samsung.com>

--- drivers/media/video/s5p-mfc/s5p_mfc.c
+++ drivers/media/video/s5p-mfc/s5p_mfc.c
@@ -1938,12 +1938,6 @@ static int s5p_mfc_enc_buf_prepare(struct vb2_buffer *vb)
 			vb2_plane_size(vb, 0), ctx->luma_size);
 		mfc_debug("plane size: %ld, luma size: %d\n",
 			vb2_plane_size(vb, 1), ctx->chroma_size);
-
-		if (vb2_plane_size(vb, 0) < ctx->luma_size ||
-		    vb2_plane_size(vb, 1) < ctx->chroma_size) {
-			mfc_err("plane size is too small for output\n");
-			return -EINVAL;
-		}
 	} else {
 		mfc_err("inavlid queue type: %d\n", vq->type);
 		return -EINVAL;
