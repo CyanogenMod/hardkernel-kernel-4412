052085fc49c00e1e9b2a418674fc533f68069585
Huisung Kang <hs1218.kang@samsung.com>
2011-02-18 11:04:41 +0900

    ARM: S5PC11X: Use PHYS_OFFSET as the 0x30000000 when S5PC11X chip
    This patch modify base address of SDRAM in case of S5PC11X.
    (S5PC11X : 0x30000000, S5PV210: 0x20000000)
    
    Change-Id: I33c5fdedf5142c0aee5ba23d39d29e3a0e60dc52
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-s5pv210/include/mach/map.h
+++ arch/arm/mach-s5pv210/include/mach/map.h
@@ -16,7 +16,11 @@
 #include <plat/map-base.h>
 #include <plat/map-s5p.h>
 
+#ifdef CONFIG_CPU_S5PC110
+#define S5PV210_PA_SDRAM		0x30000000
+#else
 #define S5PV210_PA_SDRAM		0x20000000
+#endif
 
 #define S5PV210_PA_SROM_BANK5		0xA8000000
 
