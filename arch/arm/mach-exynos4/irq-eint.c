434bf441d57ae6e689481b8b9bfd9a49da77311b
Huisung Kang <hs1218.kang@samsung.com>
2011-04-16 15:07:40 +0900

    ARM: EXYNOS4: IRQ: Can handle edge interrupt handler
    While wake up, if external interrupt uses handle_level_irq as handle_irq
    (it actually uses edge type irq), That interrupt is missed.
    So, if external uses edge type irq, it should use handle_edge_irq.
    
    Change-Id: I8c9c68f2b2ffbbb4e577cd468d0c7ae250c728db
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-exynos4/irq-eint.c
+++ arch/arm/mach-exynos4/irq-eint.c
@@ -88,6 +88,7 @@ static int exynos4_irq_eint_set_type(struct irq_data *data, unsigned int type)
 	int shift;
 	u32 ctrl, mask;
 	u32 newvalue = 0;
+	struct irq_desc *desc = irq_to_desc(data->irq);
 
 	switch (type) {
 	case IRQ_TYPE_EDGE_RISING:
@@ -141,6 +142,11 @@ static int exynos4_irq_eint_set_type(struct irq_data *data, unsigned int type)
 	default:
 		printk(KERN_ERR "No such irq number %d", offs);
 	}
+
+	if (type & IRQ_TYPE_EDGE_BOTH)
+		desc->handle_irq = handle_edge_irq;
+	else
+		desc->handle_irq = handle_level_irq;
 
 	return 0;
 }
