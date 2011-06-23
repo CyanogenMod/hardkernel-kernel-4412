490ff979caf452a2b06d4f785fea3cc314ed5c5b
Seungwon Jeon <tgih.jun@samsung.com>
2011-06-24 05:12:56 +0900

    ARM: EXYNOS4: Define platform device for SPI.
    This patch adds platform device for SPI in EXYNOS4.
    
    Change-Id: Ic94a683007ed245e5ef04de4fa506250a6558069
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -83,6 +83,9 @@ extern struct platform_device s5pv210_device_spi0;
 extern struct platform_device s5pv210_device_spi1;
 extern struct platform_device s5p64x0_device_spi0;
 extern struct platform_device s5p64x0_device_spi1;
+extern struct platform_device exynos4_device_spi0;
+extern struct platform_device exynos4_device_spi1;
+extern struct platform_device exynos4_device_spi2;
 
 extern struct platform_device s3c_device_hwmon;
 
