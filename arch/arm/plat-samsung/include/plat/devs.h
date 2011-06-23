0c12d2f29627c3f4d2adb355617c392a26b19ec0
Youngmok Song <ym.song@samsung.com>
2011-06-24 02:34:17 +0900

    ARM: S5P: Add platform device for MIPI DSI
    This patch add platform devices file for MIPI DSI
    
    Change-Id: Ic9f05bd052c22d0fbc00ed4200c7d6d30db73129
    Signed-off-by: Youngmok Song <ym.song@samsung.com>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -180,7 +180,7 @@ extern struct platform_device s5p_device_ohci;
 extern struct platform_device exynos4_device_sysmmu[];
 
 extern struct platform_device s5p_device_mfc;
-
+extern struct platform_device s5p_device_dsim;
 /* s3c2440 specific devices */
 
 #ifdef CONFIG_CPU_S3C2440
