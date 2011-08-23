64d6e9b95c6219a30b52a3eba8720f560b8b308a
Seungwon Jeon <tgih.jun@samsung.com>
2011-08-23 18:42:17 +0900

    ARM: EXYNOS4: Fix the clock rate of host.
    Setting value of host clock rate is corrected.
    
    Change-Id: I45114eaf21e399658714b719bd09f7acbbf5bd71
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>

--- arch/arm/mach-exynos/mach-smdkv310.c
+++ arch/arm/mach-exynos/mach-smdkv310.c
@@ -608,7 +608,7 @@ static void exynos4_dwmci_cfg_gpio(int width)
 static struct dw_mci_board exynos4_dwmci_pdata __initdata = {
 	.num_slots = 1,
 	.quirks = DW_MCI_QUIRK_BROKEN_CARD_DETECTION | DW_MCI_QUIRK_HIGHSPEED,
-	.bus_hz = 160 * 1000 * 1000,
+	.bus_hz = 80 * 1000 * 1000,
 	.caps = MMC_CAP_UHS_DDR50 | MMC_CAP_1_8V_DDR | MMC_CAP_8_BIT_DATA,
 	.detect_delay_ms = 200,
 	.hclk_name = "dwmci",
