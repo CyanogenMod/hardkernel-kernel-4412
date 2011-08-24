b91de49df983c8620c6ff2fac7d7d9fbedfa2c04
Jonghwan Choi <jhbird.choi@samsung.com>
2011-08-24 09:54:43 +0900

    ARM: EXYNOS: Remove useless code ralated to max8649
    Max8649 is not used in smdk4212.
    
    Change-Id: I635f021b8c7a27e25d9dc3d250e9234c55e70bbe
    Signed-off-by: Jonghwan Choi <jhbird.choi@samsung.com>

--- arch/arm/mach-exynos/mach-smdk4212.c
+++ arch/arm/mach-exynos/mach-smdk4212.c
@@ -185,29 +185,18 @@ static void __init smdk4212_usbgadget_init(void)
 }
 #endif
 
-/* max8649 */
 static struct regulator_consumer_supply max8952_supply =
-	REGULATOR_SUPPLY("vdd_arm", NULL);
-
-static struct regulator_consumer_supply max8952m_supply =
 	REGULATOR_SUPPLY("vdd_mif", NULL);
-
-static struct regulator_consumer_supply max8649_supply =
-	REGULATOR_SUPPLY("vdd_int", NULL);
-
-static struct regulator_consumer_supply max8649a_supply =
-	REGULATOR_SUPPLY("vdd_g3d", NULL);
 
 static struct regulator_init_data max8952_init_data = {
 	.constraints	= {
-		.name		= "vdd_arm range",
-		.min_uV		= 770000,
-		.max_uV		= 1400000,
+		.name		= "vdd_mif range",
+		.min_uV		= 1100000,
+		.max_uV		= 1100000,
 		.always_on	= 1,
-		.boot_on	= 1,
 		.valid_ops_mask	= REGULATOR_CHANGE_VOLTAGE,
 		.state_mem	= {
-			.uV		= 1200000,
+			.uV		= 1100000,
 			.disabled	= 1,
 		},
 	},
@@ -215,84 +204,11 @@ static struct regulator_init_data max8952_init_data = {
 	.consumer_supplies	= &max8952_supply,
 };
 
-static struct regulator_init_data max8952m_init_data = {
-	.constraints	= {
-		.name		= "vdd_mif range",
-		.min_uV		= 1100000,
-		.max_uV		= 1100000,
-		.always_on	= 1,
-		.boot_on	= 1,
-		.valid_ops_mask	= REGULATOR_CHANGE_VOLTAGE,
-		.state_mem	= {
-			.uV		= 1100000,
-			.disabled	= 1,
-		},
-	},
-	.num_consumer_supplies	= 1,
-	.consumer_supplies	= &max8952m_supply,
-};
-
-static struct regulator_init_data max8649_init_data = {
-	.constraints	= {
-		.name		= "vdd_int range",
-		.min_uV		= 750000,
-		.max_uV		= 1380000,
-		.always_on	= 1,
-		.boot_on	= 1,
-		.valid_ops_mask	= REGULATOR_CHANGE_VOLTAGE,
-		.state_mem	= {
-			.uV		= 1100000,
-			.disabled	= 1,
-		},
-	},
-	.num_consumer_supplies	= 1,
-	.consumer_supplies	= &max8649_supply,
-};
-
-static struct regulator_init_data max8649a_init_data = {
-	.constraints	= {
-		.name		= "vdd_g3d range",
-		.min_uV		= 750000,
-		.max_uV		= 1380000,
-		.always_on	= 0,
-		.boot_on	= 0,
-		.valid_ops_mask	= REGULATOR_CHANGE_VOLTAGE |
-				  REGULATOR_CHANGE_STATUS,
-		.state_mem	= {
-			.uV		= 1200000,
-			.disabled	= 1,
-		},
-	},
-	.num_consumer_supplies	= 1,
-	.consumer_supplies	= &max8649a_supply,
-};
-
-static struct max8649_platform_data __maybe_unused exynos4_max8952_info = {
-	.mode		= 3,	/* VID1 = 1, VID0 = 1 */
-	.extclk		= 0,
-	.ramp_timing	= MAX8649_RAMP_32MV,
-	.regulator	= &max8952_init_data,
-};
-
-static struct max8649_platform_data exynos4_max8952m_info = {
+static struct max8649_platform_data exynos4_max8952_info = {
 	.mode		= 1,	/* VID1 = 0, VID0 = 1 */
 	.extclk		= 0,
 	.ramp_timing	= MAX8649_RAMP_32MV,
-	.regulator	= &max8952m_init_data,
-};
-
-static struct max8649_platform_data exynos4_max8649_info = {
-	.mode		= 2,	/* VID1 = 1, VID0 = 0 */
-	.extclk		= 0,
-	.ramp_timing	= MAX8649_RAMP_32MV,
-	.regulator	= &max8649_init_data,
-};
-
-static struct max8649_platform_data exynos4_max8649a_info = {
-	.mode		= 2,	/* VID1 = 1, VID0 = 0 */
-	.extclk		= 0,
-	.ramp_timing	= MAX8649_RAMP_32MV,
-	.regulator	= &max8649a_init_data,
+	.regulator	= &max8952_init_data,
 };
 
 /* max8997 */
@@ -510,9 +426,6 @@ static struct wm8994_pdata wm8994_platform_data = {
 
 static struct i2c_board_info i2c_devs0[] __initdata = {
 	{
-		I2C_BOARD_INFO("max8649", 0x62),
-		.platform_data	= &exynos4_max8649a_info,
-	}, {
 		I2C_BOARD_INFO("max8997", 0x66),
 		.platform_data	= &exynos4_max8997_info,
 	}
@@ -522,9 +435,6 @@ static struct i2c_board_info i2c_devs1[] __initdata = {
 	{
 		I2C_BOARD_INFO("wm8994", 0x1a),
 		.platform_data	= &wm8994_platform_data,
-	}, {
-		I2C_BOARD_INFO("max8649", 0x60),
-		.platform_data	= &exynos4_max8649_info,
 	},
 #ifdef CONFIG_VIDEO_TVOUT
 	{
@@ -536,7 +446,7 @@ static struct i2c_board_info i2c_devs1[] __initdata = {
 static struct i2c_board_info i2c_devs3[] __initdata = {
 	{
 		I2C_BOARD_INFO("max8952", 0x60),
-		.platform_data	= &exynos4_max8952m_info,
+		.platform_data	= &exynos4_max8952_info,
 	},
 };
 
