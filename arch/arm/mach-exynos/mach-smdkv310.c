3a1b497b01ee178ceb74941071c53f560c7114f5
Jeongtae Park <jtp.park@samsung.com>
2011-09-03 20:33:54 +0900

    ARM: EXYNOS: Change the name of MFC device in the CMA map
    This patch change the name of MFC device in the CMA
    map definition on SMDKV310.
    
    Change-Id: I6aa9b93d944337fb44d3181f71fe367a980d6b40
    Signed-off-by: Jeongtae Park <jtp.park@samsung.com>

--- arch/arm/mach-exynos/mach-smdkv310.c
+++ arch/arm/mach-exynos/mach-smdkv310.c
@@ -2296,7 +2296,7 @@ static void __init exynos4_reserve_mem(void)
 		"s3c-fimc.0=fimc0;s3c-fimc.1=fimc1;s3c-fimc.2=fimc2;s3c-fimc.3=fimc3;"
 		"exynos4210-fimc.0=fimc0;exynos4210-fimc.1=fimc1;exynos4210-fimc.2=fimc2;exynos4210-fimc.3=fimc3;"
 #if !defined(CONFIG_VIDEO_SAMSUNG_S5P_MFC)
-		"s5p-mfc=mfc,mfc0,mfc1;"
+		"s3c-mfc=mfc,mfc0,mfc1;"
 #else
 		"mfc=mfc,mfc0,mfc1;"
 		"s5p-mfc/f=fw;"
