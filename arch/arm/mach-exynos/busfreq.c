59eee1930ee9b0e4b91c2cf73c942f2a38bc2346
Jonghwan Choi <jhbird.choi@samsung.com>
2011-09-28 07:00:46 +0900

    ARM: EXYNOS: Only support exynos4210
    Change-Id: I25b9586a8be4f5f8bb56fc30eb4000f5576e5413
    Signed-off-by: Jonghwan Choi <jhbird.choi@samsung.com>

--- arch/arm/mach-exynos/busfreq.c
+++ arch/arm/mach-exynos/busfreq.c
@@ -545,7 +545,7 @@ static int __init busfreq_mon_init(void)
 	unsigned int freq;
 	unsigned int val;
 
-	if (cpu_is_exynos4212() || cpu_is_exynos4412())
+	if (!cpu_is_exynos4210())
 		return -ENODEV;
 
 	val = __raw_readl(S5P_VA_DMC0 + 0x4);
