468a48526a5fc76cb16c757a52b86fef94c2aee6
Anton Tikhomirov <av.tikhomirov@samsung.com>
2011-09-30 20:56:36 +0900

    ARM: SAMSUNG: Fix USB3 EXYNOS DRD platform device
    This commit changes platform device definition to support host and
    device modes of USB3 EXYNOS DRD controller.
    
    Change-Id: I7402b356af2fcea80423d40c0b1e9aaa8a7912c1
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- arch/arm/plat-samsung/dev-usb3-exynos-drd.c
+++ arch/arm/plat-samsung/dev-usb3-exynos-drd.c
@@ -1,9 +1,9 @@
-/* arch/arm/plat-samsung/dev-usb3-dwc-udc.c
+/* arch/arm/plat-samsung/dev-usb3-dwc-drd.c
  *
  * Copyright (c) 2011 Samsung Electronics Co. Ltd
  * Author: Anton Tikhomirov <av.tikhomirov@samsung.com>
  *
- * Device definition for DWC SuperSpeed USB 3.0 Device Controller
+ * Device definition for EXYNOS SuperSpeed USB 3.0 DRD Controller
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -20,10 +20,10 @@
 
 #include <plat/devs.h>
 
-static struct resource exynos_ss_udc_resources[] = {
+static struct resource exynos_ss_drd_resources[] = {
 	[0] = {
-		.start	= EXYNOS5_PA_SS_UDC,
-		.end	= EXYNOS5_PA_SS_UDC + 0xB00 - 1,
+		.start	= EXYNOS5_PA_SS_DRD,
+		.end	= EXYNOS5_PA_SS_DRD + 0x100000 - 1,
 		.flags	= IORESOURCE_MEM,
 	},
 	[1] = {
@@ -38,8 +38,8 @@ static u64 exynos_ss_udc_dmamask = DMA_BIT_MASK(32);
 struct platform_device exynos_device_ss_udc = {
 	.name		= "exynos-ss-udc",
 	.id		= -1,
-	.num_resources	= ARRAY_SIZE(exynos_ss_udc_resources),
-	.resource	= exynos_ss_udc_resources,
+	.num_resources	= ARRAY_SIZE(exynos_ss_drd_resources),
+	.resource	= exynos_ss_drd_resources,
 	.dev		= {
 		.dma_mask		= &exynos_ss_udc_dmamask,
 		.coherent_dma_mask	= DMA_BIT_MASK(32),
