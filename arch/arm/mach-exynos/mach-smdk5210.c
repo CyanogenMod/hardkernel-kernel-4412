c85fea0c73458102147a02100f097231eecab0af
Anton Tikhomirov <av.tikhomirov@samsung.com>
2011-10-11 11:28:28 +0900

    ARM: EXYNOS: Register USB3 EXYNOS UDC platform device
    This commmit registers USB3 EXYNOS UDC platform device for SMDK5210
    board.
    
    Change-Id: Idcf6a5bf29e8b85af88a167bb29b36b4e107fa33
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- arch/arm/mach-exynos/mach-smdk5210.c
+++ arch/arm/mach-exynos/mach-smdk5210.c
@@ -766,8 +766,8 @@ static struct platform_device *smdk5210_devices[] __initdata = {
 	&s3c_device_spi_gpio,
 #elif defined(CONFIG_LCD_WA101S)
 	&smdk5210_lcd_wa101s,
-#endif
-#endif
+#endif	
+#endif	
 #if defined(CONFIG_VIDEO_SAMSUNG_S5P_MFC)
 	&s5p_device_mfc,
 #endif
@@ -821,6 +821,9 @@ static struct platform_device *smdk5210_devices[] __initdata = {
 #ifdef CONFIG_VIDEO_EXYNOS_MIXER
 	&s5p_device_mixer,
 #endif
+#endif
+#if defined(CONFIG_EXYNOS_DEV_SS_UDC)
+	&exynos_device_ss_udc,
 #endif
 };
 
