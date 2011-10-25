f5ae77f5e5c243a94e4f3b5dcfba53fa9c548f7d
Anton Tikhomirov <av.tikhomirov@samsung.com>
2011-10-25 18:21:48 +0900

    USB: GADGET: Register USB3 EXYNOS UDC
    This commit registers USB3 EXYNOS UDC in gadget subsystem.
    
    Change-Id: Iba06be009ca3256df910eab0b4ac29a2220dc186
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -148,6 +148,12 @@
 #define gadget_is_s3c(g)	0
 #endif
 
+#ifdef CONFIG_USB_EXYNOS_SS_UDC
+#define gadget_is_exynos_ss_udc(g) (!strcmp("exynos-ss-udc", (g)->name))
+#else
+#define gadget_is_exynos_ss_udc(g) 0
+#endif
+
 #ifdef CONFIG_USB_GADGET_EG20T
 #define	gadget_is_pch(g)	(!strcmp("pch_udc", (g)->name))
 #else
@@ -231,6 +237,8 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x29;
 	else if (gadget_is_s3c_hsudc(gadget))
 		return 0x30;
+	else if (gadget_is_exynos_ss_udc(gadget))
+		return 0x31;
 
 	return -ENOENT;
 }
