5d4757f31f3e715943d36e23200a0322ce420bf1
youngmok song <ym.song@samsung.com>
2011-11-30 08:27:50 +0900

    ARM: S5P: add definition to support toshiba mipi 2 lvds
    Change-Id: I3ae4bac0b941fed9a4cba358de835b51ecc3e768
    Signed-off-by: youngmok song <ym.song@samsung.com>

--- arch/arm/plat-s5p/include/plat/mipi_dsi.h
+++ arch/arm/plat-s5p/include/plat/mipi_dsi.h
@@ -16,6 +16,8 @@
 extern struct mipi_dsim_lcd_driver s6e8ab0_mipi_lcd_driver;
 #elif defined (CONFIG_LCD_MIPI_S6E63M0)
 extern struct mipi_dsim_lcd_driver s6e63m0_mipi_lcd_driver;
+#elif defined (CONFIG_LCD_MIPI_TC358764)
+extern struct mipi_dsim_lcd_driver tc358764_mipi_lcd_driver;
 #endif
 
 extern int s5p_mipi_dsi_wr_data(struct mipi_dsim_device *dsim,
