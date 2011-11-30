2731c64bbb819738d44a939a1f66d5b5f80871cd
Boojin Kim <boojin.kim@samsung.com>
2011-11-30 16:36:46 +0900

    ARM: MM: Add the performance measurement for cache flush all
    Change-Id: I3822bbfabac865050d181198671087511c0c8218
    Signed-off-by: Boojin Kim <boojin.kim@samsung.com>

--- arch/arm/mm/cache_perf.c
+++ arch/arm/mm/cache_perf.c
@@ -24,7 +24,7 @@
 #include <asm/outercache.h>
 #include <asm/cacheflush.h>
 
-static unsigned int try_cnt = 256;
+static unsigned int try_cnt = 1;
 module_param(try_cnt, uint, S_IRUGO);
 MODULE_PARM_DESC(try_cnt, "Try count to test");
 
@@ -36,7 +36,7 @@ static bool l2 = 1;
 module_param(l2, bool, S_IRUGO);
 MODULE_PARM_DESC(l2, "Set for L2 check");
 
-static bool mset = 1;
+static bool mset;
 module_param(mset, bool, S_IRUGO);
 MODULE_PARM_DESC(mset, "Set for Memset");
 
@@ -52,13 +52,17 @@ static bool fops = 1;
 module_param(fops, bool, S_IRUGO);
 MODULE_PARM_DESC(fops, "Set for Flush");
 
+static bool all = 1;
+module_param(all, bool, S_IRUGO);
+MODULE_PARM_DESC(all, "Set for all flush");
+
 struct task_struct *cacheperf_task;
 static bool thread_running;
 
 #define START_SIZE (32)
 #define END_SIZE (SZ_2M)
 
-static void print_result(char *ops, u32 xfer_size, struct timespec lhs,
+static void print_result(u32 xfer_size, struct timespec lhs,
 			 struct timespec rhs)
 {
 	long us;
@@ -67,10 +71,18 @@ static void print_result(char *ops, u32 xfer_size, struct timespec lhs,
 	ts = timespec_sub(rhs, lhs);
 	us = ts.tv_sec*USEC_PER_SEC + ts.tv_nsec/NSEC_PER_USEC;
 
-	if (!strcmp(ops, "memset"))
-		printk(KERN_INFO "%u: %lu\n", xfer_size, us);
-	else
-		printk(KERN_INFO "%lu\n", us);
+	printk(KERN_INFO "%u: %lu\n", xfer_size, us);
+}
+
+static long update_timeval(struct timespec lhs, struct timespec rhs)
+{
+	long val;
+	struct timespec ts;
+
+	ts = timespec_sub(rhs, lhs);
+	val = ts.tv_sec*NSEC_PER_SEC + ts.tv_nsec;
+
+	return val;
 }
 
 bool buf_compare(u32 src[], u32 dst[], unsigned int bytes)
@@ -97,89 +109,119 @@ static int cacheperf(void)
 	u32 bufend;
 	struct timespec beforets;
 	struct timespec afterts;
-
+	long timeval;
 	vbuf = kmalloc(END_SIZE, GFP_KERNEL);
 	pbuf = virt_to_phys(vbuf);
 
 	if (mset) {
-		printk(KERN_INFO "## Memset perf (us)\n");
+		printk(KERN_INFO "## Memset perf (ns)\n");
 
 		xfer_size = START_SIZE;
-		while (xfer_size < END_SIZE) {
-			xfer_size *= 2;
+		while (xfer_size <= END_SIZE) {
 			bufend = pbuf + xfer_size;
 
 			getnstimeofday(&beforets);
 			for (i = 0; i < try_cnt; i++)
 				memset(vbuf, i, xfer_size);
 			getnstimeofday(&afterts);
-			print_result("memset", xfer_size, beforets, afterts);
+			print_result(xfer_size, beforets, afterts);
+			xfer_size *= 2;
 		}
 	}
 
 	if (cops) {
-		printk(KERN_INFO "## Clean perf (us)\n");
+		printk(KERN_INFO "## Clean perf (ns)\n");
 
 		xfer_size = START_SIZE;
-		while (xfer_size < END_SIZE) {
-			xfer_size *= 2;
+		while (xfer_size <= END_SIZE) {
 			bufend = pbuf + xfer_size;
+			timeval = 0;
 
-			getnstimeofday(&beforets);
 			for (i = 0; i < try_cnt; i++) {
 				memset(vbuf, i, xfer_size);
+				getnstimeofday(&beforets);
 				if (l1)
 					dmac_map_area(vbuf, xfer_size,
 							DMA_TO_DEVICE);
 				if (l2)
 					outer_clean_range(pbuf, bufend);
+				getnstimeofday(&afterts);
+				timeval += update_timeval(beforets, afterts);
+				xfer_size *= 2;
 			}
-			getnstimeofday(&afterts);
-			print_result("clean", xfer_size, beforets, afterts);
+			printk(KERN_INFO "%lu\n", timeval);
 		}
 	}
 
 	if (iops) {
-		printk(KERN_INFO "## Invalidate perf (us)\n");
+		printk(KERN_INFO "## Invalidate perf (ns)\n");
 
 		xfer_size = START_SIZE;
-		while (xfer_size < END_SIZE) {
-			xfer_size *= 2;
+		while (xfer_size <= END_SIZE) {
 			bufend = pbuf + xfer_size;
+			timeval = 0;
 
-			getnstimeofday(&beforets);
 			for (i = 0; i < try_cnt; i++) {
 				memset(vbuf, i, xfer_size);
+				getnstimeofday(&beforets);
 				if (l2)
 					outer_inv_range(pbuf, bufend);
 				if (l1)
 					dmac_unmap_area(vbuf, xfer_size,
 							DMA_FROM_DEVICE);
+				getnstimeofday(&afterts);
+				timeval += update_timeval(beforets, afterts);
+				xfer_size *= 2;
 			}
-			getnstimeofday(&afterts);
-			print_result("inval", xfer_size, beforets, afterts);
+			printk(KERN_INFO "%lu\n", timeval);
 		}
 	}
 
 	if (fops) {
-		printk(KERN_INFO "## Flush perf (us)\n");
+		printk(KERN_INFO "## Flush perf (ns)\n");
 
 		xfer_size = START_SIZE;
-		while (xfer_size < END_SIZE) {
-			xfer_size *= 2;
+		while (xfer_size <= END_SIZE) {
 			bufend = pbuf + xfer_size;
+			timeval = 0;
 
-			getnstimeofday(&beforets);
 			for (i = 0; i < try_cnt; i++) {
 				memset(vbuf, i, xfer_size);
+				getnstimeofday(&beforets);
 				if (l1)
 					dmac_flush_range(vbuf,
 					(void *)((u32) vbuf + xfer_size));
 				if (l2)
 					outer_flush_range(pbuf, bufend);
+				getnstimeofday(&afterts);
+				timeval += update_timeval(beforets, afterts);
+				xfer_size *= 2;
 			}
-			getnstimeofday(&afterts);
-			print_result("flush", xfer_size, beforets, afterts);
+			printk(KERN_INFO "%lu\n", timeval);
+		}
+	}
+
+
+	if (all) {
+		printk(KERN_INFO "## Flush all perf (ns)\n");
+
+		xfer_size = START_SIZE;
+		while (xfer_size <= END_SIZE) {
+			bufend = pbuf + xfer_size;
+			timeval = 0;
+
+			for (i = 0; i < try_cnt; i++) {
+				memset(vbuf, i, xfer_size);
+				getnstimeofday(&beforets);
+					if (l1)
+						flush_cache_all();
+					if (l2)
+						outer_flush_all();
+				getnstimeofday(&afterts);
+				timeval += update_timeval(beforets, afterts);
+				xfer_size *= 2;
+			}
+			printk(KERN_INFO "%lu\n", timeval);
 		}
 	}
 
@@ -203,8 +245,8 @@ int __init cacheperf_init(void)
 	l2 = 0;
 #endif
 
-	printk(KERN_ERR "Test condition: l1: %d, l2: %d, try_cnt:%d\n",
-				l1, l2, try_cnt);
+	printk(KERN_ERR "Test condition: l1: %d, l2: %d, try_cnt:%d, (%dB ~ %dMB)\n",
+				l1, l2, try_cnt, START_SIZE, END_SIZE/SZ_1M);
 
 	cacheperf_task = kzalloc(sizeof(struct task_struct), GFP_KERNEL);
 	cacheperf_task = kthread_run(thread_func, NULL, "cacheperf_thread");
