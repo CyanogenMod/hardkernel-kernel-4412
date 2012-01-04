be31dba54a6198b15bb8f213d3925a862c73f1af
Huisung Kang <hs1218.kang@samsung.com>
2012-01-04 11:52:19 +0900

    ARM: EXYNOS: Fixup prevent bug related to 'Unchecked return value'
    In s5p_pmu_init(): Value returned from a function is not checked for
    		   errors before being used (CWE-252).
    
    Change-Id: I6868ff60eeea45f83d6d7b728ded741ac0661c33
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/plat-s5p/dev-pmu.c
+++ arch/arm/plat-s5p/dev-pmu.c
@@ -50,7 +50,12 @@ struct platform_device s5p_device_pmu = {
 
 static int __init s5p_pmu_init(void)
 {
-	platform_device_register(&s5p_device_pmu);
+	int ret;
+
+	ret = platform_device_register(&s5p_device_pmu);
+	if (ret)
+		pr_warning("s5p_pmu_init: pmu device not registered.\n");
+
 	return 0;
 }
 arch_initcall(s5p_pmu_init);
