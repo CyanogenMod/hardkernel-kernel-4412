31efc911370f0b5e14083db76a5e920d627b4f23
Lee InGeol <ig1004.lee@samsung.com>
2012-07-09 11:54:23 +0900

    MEDIA: G3D: Add asv setting for prime efuse.
    Add asv setting for prime efuse.
    It checks flag bit. If true, asv table is default +25mV.
    
    Change-Id: I34fc0448d56ff3417956ecca66a83d4e2b0e550b
    Signed-off-by: Lee InGeol <ig1004.lee@samsung.com>

--- drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform_dvfs.c
+++ drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform_dvfs.c
@@ -392,6 +392,7 @@ static mali_bool mali_dvfs_table_update(void)
 		step_num = MALI_DVFS_STEPS-1;
 
 	if(soc_is_exynos4412()) {
+		/*check it's pega-prime or pega-Q*/
 		if(samsung_rev() < EXYNOS4412_REV_2_0) {
 			step_num = MALI_DVFS_STEPS-1;
 			for (i = 0; i < step_num; i++) {
@@ -400,6 +401,15 @@ static mali_bool mali_dvfs_table_update(void)
 				MALI_PRINT(("mali_dvfs[%d].vol = %d\n", i, mali_dvfs[i].vol));
 			}
 		}
+		/* For Pega-Prime e-fuse, add 25mV from default ASV table*/
+		else if((is_special_flag() >> G3D_LOCK_FLAG) & 0x1) {
+			for (i = 0; i < step_num; i++) {
+				MALI_PRINT((":::exynos_result_of_asv : %d\n", exynos_result_of_asv));
+				mali_dvfs[i].vol = asv_3d_volt_9_table_for_prime[i][exynos_result_of_asv] + 25000;
+				MALI_PRINT(("mali_dvfs[%d].vol = %d\n", i, mali_dvfs[i].vol));
+			}
+		}
+		/* pega-prime default ASV table */
 		else {
 			for (i = 0; i < step_num; i++) {
 				MALI_PRINT((":::exynos_result_of_asv : %d\n", exynos_result_of_asv));
